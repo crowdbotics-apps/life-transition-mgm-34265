bd3f43c6c8767e02547b2440fe867f7dbdc98853
Crowdbotics <team@crowdbotics.com>
2022-03-17 19:23:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "life_transition_mgm_34265",
+  url: "https://life_transition_mgm_34265.botics.co",
+  api: "https://life_transition_mgm_34265.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
